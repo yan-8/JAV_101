c3bec624d2a8224bee5eb3a883bfa2a73f1fd8b3
Yan <yan.bondarenko@gmail.com>
2023-07-09 00:56:26 +0200

    Make some changes

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-let myVar = "Hello World!";
-console.log(myVar);
+let a = 5;
+let b = 5.6;
+console.log(typeof a); // number
+console.log(typeof b); // number
 
-myVar = "New Hello World!"
-console.log(myVar)
+a = "String";
+console.log(typeof a); // string
+
+let isAdmin = false;
+console.log(typeof isAdmin); // boolean
+
+let x;
+console.log(typeof x); // undefined
+x = 55;
+console.log(typeof x); // number
+
+let cb = null;
+console.log(typeof cb); // object
+
+let fg = null;
+console.log(typeof (fg == null)); // boolean, because step 1 - check in brackets, step 2 - check type
+console.log(typeof fg == null); // boolean, because step 1 - check in brackets, step 2 - check type
