d878087c1aa4e7f96d47f3a58499a06deed4dff5
Yan <yan.bondarenko@gmail.com>
2023-07-24 23:07:55 +0200

    False options

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-// task 1
-let customersPotencialJobInHours = 40;
-let mySalaryPerHour = 150;
-let myJobHoursPerDay = 5;
-let myLeftWorkingDays = 8;
-let myLeftWorkingHours = myJobHoursPerDay * myLeftWorkingDays;
-let myTotalSalary = mySalaryPerHour * customersPotencialJobInHours;
+// // task 1
+// let customersPotencialJobInHours = 40;
+// let mySalaryPerHour = 150;
+// let myJobHoursPerDay = 5;
+// let myLeftWorkingDays = 8;
+// let myLeftWorkingHours = myJobHoursPerDay * myLeftWorkingDays;
+// let myTotalSalary = mySalaryPerHour * customersPotencialJobInHours;
 
-console.log("Can I work? - " + (myLeftWorkingHours >= customersPotencialJobInHours));
-console.log("My total salary is - USD " + myTotalSalary);
+// console.log("Can I work? - " + (myLeftWorkingHours >= customersPotencialJobInHours));
+// console.log("My total salary is - USD " + myTotalSalary);
+
+
+
+// let a = 2 + "10";
+// console.log(a - 10); // 200
+
+
+
+console.log(Boolean(0)); // false
+console.log(Boolean('')); // false
+let a; // undefined variable
+console.log(Boolean(a)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean(null)); // 
